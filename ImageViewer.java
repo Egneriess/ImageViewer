b91e0b42fd3772d17e1d5688d64cf9a2e8859a8c
liyupian <108216173+liyupian@users.noreply.github.com>
2022-09-19 19:26:57 +0800

    Add files via upload

---
+++ ImageViewer.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageViewer {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var frame = new ImageViewerFrame();
+            frame.setTitle("ImageViewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
